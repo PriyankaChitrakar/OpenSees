c34c84b44cd317e677a3606a90de5be62df1c7fb
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2023-02-24 15:48:52 -0800

    meeting 2

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -245,7 +245,7 @@ TDConcreteMC10NL::setCreepBasicStrain(double time, double stress)
         PHIB_i[i] = setPhiBasic(time,TIME_i[i]); //Determine PHI //ntosic: PHIB
 		eta_i[i] = setEta(time, TIME_i[i]); // Priyanka: Added for Secondary Creep
 		runSumStress += DSIG_i[i]; // Priyanka: Added for Secondary Creep
-        runSum += (PHIB_i[i]*DSIG_i[i]/Ecm)*(1+2*eta_i[i]*pow(( stress /(fc)),4)); //Priyanka: Edited for Secondary Creep//CONSTANT STRESS within Time interval //ntosic: changed to Ecm from Ec (according to Model Code formulation of phi basic)
+        runSum += (PHIB_i[i]*DSIG_i[i]/Ecm)*(1+2*eta_i[i]*pow((18.8 /(fc)),4)); //Priyanka: Edited for Secondary Creep//CONSTANT STRESS within Time interval //ntosic: changed to Ecm from Ec (according to Model Code formulation of phi basic)
     }
     
     phib_i = PHIB_i[count];
@@ -267,7 +267,7 @@ TDConcreteMC10NL::setCreepDryingStrain(double time, double stress)
 		PHID_i[i] = setPhiDrying(time, TIME_i[i]); //Determine PHI //ntosic: PHID
 		eta_i[i] = setEta(time, TIME_i[i]); // Priyanka: Added for Secondary Creep
 		runSumStress += DSIG_i[i]; // Priyanka: Added for Secondary Creep
-		runSum += (PHID_i[i]*DSIG_i[i]/Ecm)*(1+2*eta_i[i]*pow((stress /(fc)),4)); //Priyanka: Edited for Secondary Creep //CONSTANT STRESS within Time interval //ntosic: changed to Ecm from Ec (according to Model Code formulation of phi drying)
+		runSum += (PHID_i[i]*DSIG_i[i]/Ecm)*(1+2*eta_i[i]*pow((18.8 /(fc)),4)); //Priyanka: Edited for Secondary Creep //CONSTANT STRESS within Time interval //ntosic: changed to Ecm from Ec (according to Model Code formulation of phi drying)
 	}
 
 	phid_i = PHID_i[count];
