db870fa90acaff1ca8cc939a3390b032106a565f
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2023-03-24 11:36:03 -0700

    NL cout

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -252,7 +252,7 @@ TDConcreteMC10NL::setCreepBasicStrain(double time, double stress)
 		runSum += (PHIB_i[i]*DSIG_i[i]/Ecm)*(1+2*eta_i[i]*pow((stress/fc),4)); //Priyanka: Edited for Secondary Creep//CONSTANT STRESS within Time interval //ntosic: changed to Ecm from Ec (according to Model Code formulation of phi basic)
 		//runSum += (PHIB_i[i] * DSIG_i[i] / Ecm);
 		cout << "\n          PHIB_i[" << i << "]: " << PHIB_i[i] << ".";
-		cout << "\n	          runSum: " << runSum << ".";
+		cout << "\n	         runSum: " << runSum << ".";
     }
     
     phib_i = PHIB_i[count];
