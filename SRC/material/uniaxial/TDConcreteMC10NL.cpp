a5f7092b63c3d2f28fd8611d1eb736fa8b67b8c4
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2023-06-23 12:18:22 -0700

    eps=short time stain D

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -441,7 +441,7 @@ TDConcreteMC10NL::setTrialStrain(double trialStrain, double strainRate)
 				if (eps_total < ShortTimeStrainD)
 
 				{
-					//eps_total = ShortTimeStrainD;
+					eps_total = ShortTimeStrainD;
 					sig = fcu;
 					//eps_total = epscu;
 				}
