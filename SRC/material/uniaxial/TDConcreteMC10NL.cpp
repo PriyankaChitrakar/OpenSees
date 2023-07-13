999972a35126c588f4776c3f440162d27ac83794
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2023-07-13 15:44:34 -0700

    4

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -834,6 +834,7 @@ TDConcreteMC10NL::Tens_Envlp (double epsc, double &sigc, double &Ect)
 
 	double Ec0 = Ec;
 	double eps0 = ft / Ec0;
+	double Ets;
 	Ets = ft / 0.002;
 	double epsu = ft * (1.0 / Ets + 1.0 / Ec0);
 	
