35cb2bc6cfa851a89fd886fc2ed1903ae5b5e3bd
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2023-09-13 14:39:13 -0700

    try4

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -470,15 +470,9 @@ TDConcreteMC10NL::setTrialStrain(double trialStrain, double strainRate)
 
 				cout << "\n        PATH 2";
 				cout << "\n        eps_m: " << eps_m << ".";
-				if (eps_m < -0.002) { eps_m = eps_total;
-				} //Priyanka
-
-				else {
-					eps_m = eps_total - eps_crb - eps_crd - eps_shb - eps_shd;
-				} //ntosic} //Priyanka
-
-        		
-				sig = setStress(eps_m, e);
+						
+					eps_m = eps_total - eps_crb - eps_crd - eps_shb - eps_shd;  //ntosic
+					sig = setStress(eps_m, e);
 				
 				
 				
