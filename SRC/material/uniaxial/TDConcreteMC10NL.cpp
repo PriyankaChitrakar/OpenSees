6f3863dcb7c97ed78a0e35220d448c08d5ee11c9
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2023-09-22 14:43:32 -0700

    try2

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -256,7 +256,7 @@ TDConcreteMC10NL::setCreepBasicStrain(double time, double stress, double eo)
 		//cout << "\n          eta_i[" << i << "]: " << eta_i[i] << ".";
 		cout << "\n          DSIG_i[" << i << "]: " << DSIG_i[i] << ".";
 		ShortTimeStrain = setShortTimeStrain(DSIG_i[i]); //Priyanka
-		cout << "\n          eps_m_i[" << i << "]: " << eps_m[i] << ".";
+		
 
 		cout << "\n	         eo: " << eo << ".";
 		if (eo < -0.002) 
