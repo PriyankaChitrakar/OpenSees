6594d875044e050bd3d5e137c1e8048d00efb5e8
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2024-04-16 13:35:27 -0700

    5.4. try

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -1054,23 +1054,23 @@ TDConcreteMC10NL::Compr_Envlp(double epsc, double& sigc, double& Ect)
 	!   sigc  = current stress
 	!   Ect   = tangent concrete modulus
 	-----------------------------------------------------------------------*/
-	double epsc0 = cem;
-	//double epsc02 = epsc01 * phibb; //Priyanka////phibb=k; 
-	//double kfc = fc * phibb;
+	double epsc01 = cem;
+	double epsc0 = epsc01 * phibb; //Priyanka////phibb=k; 
+	double kfc = fc * phibb;
 	double Ec0 = Ec; //ntosic
 	
 
 	double ratLocal = epsc / epsc0;
 	if (epsc >= epsc0) {
-		sigc = fc * ratLocal * (2.0 - ratLocal);
+		sigc = kfc * ratLocal * (2.0 - ratLocal);
 		Ect = Ec0 * (1.0 - ratLocal);
 	}
 	else {
 
 		//   linear descending branch between epsc0 and epscu
 		if (epsc > epscu) {
-			sigc = (fcu - fc) * (epsc - epsc0) / (epscu - epsc0) + fc;
-			Ect = (fcu - fc) / (epscu - epsc0);
+			sigc = (fcu - kfc) * (epsc - epsc0) / (epscu - epsc0) + kfc;
+			Ect = (fcu - kfc) / (epscu - epsc0);
 		}
 		else {
 
