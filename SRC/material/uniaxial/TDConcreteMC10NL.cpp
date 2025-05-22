9795151c28114f5fb78c5158c26feb00122ac45d
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2025-05-22 14:15:38 -0700

    cout

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -525,12 +525,11 @@ TDConcreteMC10NL::setTrialStrain(double trialStrain, double strainRate)
 
 				eps_m = eps_total - eps_crb - eps_crd - eps_shb - eps_shd;  //ntosic
 				sig = setStress(eps_m, e);
-				/*
+				
 				if (failure2 == 1) 
 				{					
 					sig = fcu;					
 				}
-				*/
 
 				///cout << "\n a_i[i] : " << a_i[i] << ".";
 
