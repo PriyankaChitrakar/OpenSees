45c932bd73d6f56178eb9f4d263d372cf4b98aac
Priyanka Chitrakar <chitraka@unlv.nevada.edu>
2025-06-24 09:02:50 -0700

    GL2000 Age effect modification

--- SRC/material/uniaxial/TDConcreteMC10NL.cpp
+++ SRC/material/uniaxial/TDConcreteMC10NL.cpp
@@ -386,7 +386,7 @@ TDConcreteMC10NL::setPhiBasic(double time, double tp)
 	//cout << "\n	         tmtp: " << tmtp << ".";//deci
 	
 	
-	double phiBasic = (Ec / Ecm) * (pow(1 - pow(((tp - phiba) / (tp - phiba + phidb)), 0.5), 0.5)) * ((2 * pow(tmtp, 0.3) / ((pow(tmtp, 0.3)) + 14))+ (pow(7 / tp, 0.5) * pow(tmtp / (tmtp + 7), 0.5))); //
+	double phiBasic = (Ec / Ecm) * (2*(pow(1 - pow(((tp - phiba) / (tp - phiba + phidb)), 0.5), 0.5))-0.5) * ((2 * pow(tmtp, 0.3) / ((pow(tmtp, 0.3)) + 14))+ (pow(7 / tp, 0.5) * pow(tmtp / (tmtp + 7), 0.5))); //
 	return phiBasic;
 }
 //ntosic
@@ -396,7 +396,7 @@ TDConcreteMC10NL::setPhiDrying(double time, double tp)
 	// ntosic: Model Code 2010 Equations
 	double tmtp = time - tp;
 	//double phiDrying = (Ec / Ecm) * (pow(1 - pow(((tp - phiba) / (tp - phiba + phidb)), 0.5), 0.5)) * (phida * pow(tmtp / (tmtp + phidb), 0.5));
-	double phiDrying = (Ec / Ecm) * (pow(1 - pow(((tp - phiba) / (tp - phiba + phidb)), 0.5), 0.5)) * (phida * pow(tmtp, 0.5) / pow(tmtp + phidb, 0.5));
+	double phiDrying = (Ec / Ecm) * (2*(pow(1 - pow(((tp - phiba) / (tp - phiba + phidb)), 0.5), 0.5))-0.5) * (phida * pow(tmtp, 0.5) / pow(tmtp + phidb, 0.5));
 	///cout << "\n	         phiDrying: " << phiDrying << ".";
 	return phiDrying;
 }
